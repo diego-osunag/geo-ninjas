0068add85cbb1a04a9a90b701ad9c03a4b296bb3
diego-osunag <diego.osunag@hotmail.com>
2019-07-14 11:32:19 -0600

    Imported firebase, materializeCSS, Google Maps API, created GMap page

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBxsH1FotooU5q1nIggrhI8pCUkiPup-6g",
+    authDomain: "udemy-geo-ninjas-524c5.firebaseapp.com",
+    databaseURL: "https://udemy-geo-ninjas-524c5.firebaseio.com",
+    projectId: "udemy-geo-ninjas-524c5",
+    storageBucket: "",
+    messagingSenderId: "358780931646",
+    appId: "1:358780931646:web:91482b974759dbb8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const firebaseApp = firebase.initializeApp(config);
+
+  export default firebaseApp.firestrore()
