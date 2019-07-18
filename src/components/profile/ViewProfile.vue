26dc8f27e82b695d0095df3133b28a9c9077a8f0
diego-osunag <diego.osunag@hotmail.com>
2019-07-17 23:55:13 -0600

    Profile page added, and some final styles

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="view-profile contai">
+  <div class="view-profile container">
     <div v-if="profile" class="card">
       <h2 class="deep-purple-text center">{{ profile.alias }}'s Wall</h2>
       <ul class="comments collection">
-        <li>Comment</li>
+        <li v-for="(comment, index) in comments" :key="index">
+          <div class="deep-purple-text">{{ comment.from }}</div>
+          <div class="grey-text text-darken-2">{{ comment.content }}</div>
+        </li>
       </ul>
       <form @submit.prevent="addComment">
         <div class="field">
@@ -68,7 +71,7 @@ export default {
         this.feedback = null
         db.collection('comments').add({
           to: this.$route.params.id,
-          from: this.user.id,
+          from: this.user.alias,
           content: this.newComment,
           time: Date.now()
         }).then(() => {
@@ -81,3 +84,19 @@ export default {
   }
 }
 </script>
+
+<style>
+.view-profile .card{
+  padding: 20px;
+  margin-top: 60px;
+}
+.view-profile h2{
+  font-size: 2em;
+  margin-top: 0;
+}
+.view-profile li{
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
+</style>
+
